ca25036fc1fac2dad5bbf62d788fbc136062498a
ayooubsmatti <ayooubsmatti@gmail.com>
2021-03-09 19:04:58 +0100

    Database Relationships | One To Many & Many to Many

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,13 @@ from django.db import models
 
 # Create your models here.
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
@@ -22,8 +29,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -33,7 +44,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
-    # customer =
-    # product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
