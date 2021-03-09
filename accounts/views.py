ded601781ed6c4cad5efe4abc0a8c8b101917e4d
ayooubsmatti <ayooubsmatti@gmail.com>
2021-03-09 02:25:54 +0100

    initial commit with templates file

---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def products(request):
+    return HttpResponse("products")
+
+
+def customer(request):
+    return HttpResponse("customer")
