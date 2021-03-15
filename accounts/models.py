b8f22951b2cabd7a65f7614e59099f977ddf236c
ayooubsmatti <ayooubsmatti@gmail.com>
2021-03-15 23:12:38 +0100

    User Profile With One To One Relationship

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -11,6 +12,7 @@ class Tag(models.Model):
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -60,26 +60,41 @@ def home(request):
 @unauthenticated_user
 def registerPage(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.group.add(group)
 
-            messages.success(request, 'Accounts was created for: ' + username)
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            # Added username after video because of error returning customer name if not added
+            Customer.objects.create(
+                user=user,
+                name=user.username,
+            )
+
+            messages.success(request, 'Account was created for ' + username)
+
             return redirect('login')
 
-    context = {
-        'form': form,
-    }
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    print('ORDERS:', orders)
+
+    context = {'orders': orders, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
     return render(request, 'accounts/user.html', context)
 
 
